60f96e40403722e02a5ee36af77496d7dcbb9a15
lahwran <blendmaster1024@gmail.com>
2011-03-31 05:07:59 -0600

    it works

--- ffmpegscreencast.py
+++ ffmpegscreencast.py
@@ -5,15 +5,17 @@ import subprocess
 import sys
 import tempfile
 import re
+import time
 
 #TODO: should bother figuring out arg parsing libs
 #TODO: --nozenity option
+#TODO: switch to encode to webm
 
 def searchpath(forbinary):
     return bool([True for i in os.environ["PATH"].split(":") if os.path.exists(os.path.join(i, forbinary))])
 
 if "--help" in sys.argv or "-h" in sys.argv:
-    print "usage:",sys.argv[0],"[--audio|--noaudio] [--display <X display>] [--fps <rate>] [--window] [--output <savelocation>]"
+    print "usage:",sys.argv[0],"[--audio|--noaudio] [--display <X display>] [--fps <rate>] [--window|--fullscr] [--output <savelocation>]"
     
 if not searchpath("ffmpeg"):
     print "could not find ffmpeg, enter your password to install it"
@@ -23,7 +25,7 @@ if not searchpath("ffmpeg"):
         sys.exit(1)
 
 numre=re.compile("([0-9]+)")
-if "--window" in sys.argv:
+if "--window" in sys.argv or ("--screen" not in sys.argv and subprocess.Popen(["zenity", "--question", "--text", "Record a window?"]).wait() == 0):
     x= subprocess.Popen(["xwininfo"], stdout=subprocess.PIPE).communicate()[0]
     #these make assumptions that could go wrong on any system with an abnormal xwininfo
     windowpos = [numre.search(i).group(1) for i in x.split("\n") if "Absolute upper-left " in i]
@@ -33,9 +35,7 @@ else:
     windowpos=["0","0"]
     windowsize = [numre.search(i).group(1) for i in x.split("\n") if "Width" in i or "Height" in i]
 
-tmp = tempfile.mkstemp()
-os.fdopen(tmp[0]).close()
-tmp=tmp[1]
+tmp = tempfile.mktemp()+".mkv"
 
 command = ["ffmpeg"]
 
@@ -51,6 +51,11 @@ if audio:
     command.extend("-f alsa -ac 2 -i pulse".split(" "))
 
 command.extend("-f x11grab".split(" "))
+
+if "--output" in sys.argv:
+    output=int(sys.argv[sys.argv.index("--output")+1])
+else:
+    output = subprocess.Popen("zenity --file-selection --save --title".split(" ")+["Select final output name prefix"], stdout=subprocess.PIPE).communicate()[0]
 
 fps = 30
 if "--fps" in sys.argv:
@@ -78,4 +83,19 @@ ffcmd1.terminate()
 time.sleep(2)
 ffcmd1.kill()
 
+#uses example 1 on the reference page
+command=["ffmpeg","-i",tmp]
+#if audio:
+#these appear to be required
+command.extend(["-acodec","libfaac","-ab","128k","-ac","2"])
+command.extend("-vcodec libx264 -vpre slow -crf 22 -threads 0".split(" "))
+command.append(output+".mp4")
 
+ffcmd2 = subprocess.Popen(command, stdin=open("/dev/null")) #pipe from the bitbucket so we can keep control of console input
+zenitynotif = subprocess.Popen(["zenity","--notification","--text","Reencoding screencast..."])
+ffcmd2.wait()
+zenitynotif.terminate()
+time.sleep(0.01)
+zenitynotif.kill()
+os.unlink(tmp)
+subprocess.Popen(["nautilus",os.path.dirname(output)])
